30db1000bf3ec902522f8dc7d578d7e36aac8417
Vasavi <vasavi.gitla@yahoo.com>
2021-02-24 19:21:15 -0600

    completed Assignment3

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -24,5 +24,21 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping(value="/results")
+    public String displaySearchResults(Model model,String searchType,String searchTerm)
+    {
+        ArrayList<Job> jobs=new ArrayList<>();
+        if(searchTerm.equals("all"))
+        {
+            jobs=JobData.findAll();
+        }
+        else
+        {
+            jobs= JobData.findByColumnAndValue(searchType,searchTerm);
+        }
+        model.addAttribute("jobs",jobs);
+        model.addAttribute("columns", columnChoices);
+        return "search";
+    }
 
 }
